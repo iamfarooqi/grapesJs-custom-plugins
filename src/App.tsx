63202655b7c360ded2ac85fbbe7253c91b82831a
iamfarooqi <73984528+iamfarooqi@users.noreply.github.com>
2023-12-08 16:45:43 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Editor from "./pages/Editor";
 export const Homepage = () => {
   return (
     <div>
-      <main className="h-screen w-full flex flex-col justify-between items-center bg-[#1A2238]">
+      <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
         <h1 className="text-7xl mt-6 font-extrabold text-white tracking-widest">
           Website Builder
         </h1>
@@ -16,18 +16,6 @@ export const Homepage = () => {
             </span>
           </a>
         </button>
-        <span className="mb-4 text-white text-xl">
-          <p>
-            Powered By{' '}
-            <a
-              href="https://bawdicsoft.com/"
-              target="_blank"
-              className="hover:underline"
-            >
-              BawdicSoft Pvt.(Ltd)
-            </a>
-          </p>
-        </span>
       </main>
     </div>
   );
